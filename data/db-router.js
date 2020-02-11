451f453f7e78e2fc9a828806bf5f285283c70882
Nhan <natenguyen51294@gmail.com>
2020-02-11 13:14:23 -0800

    finished the post requests

--- data/db-router.js
+++ data/db-router.js
@@ -30,17 +30,50 @@ router.get(`/:id`, async (req, res) => {
 })
 
 router.get(`/:id/comments`, async (req, res) => {
-  const {id} = req.params;
-  try{
-    const postComment = await Posts.findPostComments(id);
-    if(!postComment) {
-      res.status(404).json({message: 'The post with the specified ID does not exists'})
+  const { id } = req.params;
+  try {
+    const postComment = await Posts.findCommentById(id);
+    if (!postComment) {
+      res.status(404).json({ message: 'The post with the specified ID does not exists' })
     } else {
       res.status(200).json(postComment)
     }
   }
   catch {
-    res.status(500).json({message: 'The comments information could not be retrieved'})
+    res.status(500).json({ message: 'The comments information could not be retrieved' })
+  }
+})
+
+router.post(`/`, async (req, res) => {
+  const post = req.body;
+  try {
+    await Posts.insert(post)
+    if (!post.title || !post.contents) {
+      res.status(400).json({ message: 'Please provide title and contents for the post' })
+    } else {
+      res.status(201).json(post);
+    }
+  }
+  catch {
+    res.status(500).json({ message: 'There was an error while saving the post to the database' })
+  }
+})
+
+router.post(`/:id/comments`, async (req, res) => {
+  const { id } = req.params;
+  const specPost = req.body;
+  try {
+    const post = Posts.findCommentById(id);
+    if (!post) {
+      res.status(404).json({ message: 'The post with the specified ID does not exists' })
+    } else if (!specPost.text) {
+      res.status(400).json({ message: 'Please provide text for the comment' })
+    } else {
+      res.status(201).json(specPost)
+    }
+  }
+  catch {
+    res.status(500).json({ message: 'There was an error while saving the comment to the database' })
   }
 })
 
